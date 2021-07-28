44554d1dc7ac5913dab91a0e537b485be4fdf7bb
Weng Ngai Lam <wengngai@u.nus.edu>
2021-07-28 23:28:03 +0800

    New codes for 3 species model (without Gironniera). Dilution factor now set at 20 to reduce overall stem density.

---
+++ code/setup_map_three spp.R
@@ -0,0 +1,215 @@
+#################
+# SETUP BASEMAP #
+#################
+
+# read in hydrological model predictions (each with 22 years: 2021-2042)
+# Scenarios
+nssf.usual <- list()
+nssf.extreme <- list()
+nssf.low <- list()
+nssf.high <- list()
+
+# read in and reset values to 0/1 (resampling issues)
+for(i in 1:22){
+    nssf.usual[[i]] <- raster("data/As usual scenario.tif", band=i)
+    values(nssf.usual[[i]]) <- ifelse(values(nssf.usual[[i]]) < 0.5, 0, 1)
+    nssf.extreme[[i]] <- raster("data/Extreme scenario.tif", band=i)
+    values(nssf.extreme[[i]]) <- ifelse(values(nssf.extreme[[i]]) < 0.5, 0, 1)
+    nssf.low[[i]] <- raster("data/Low RF scenario.tif", band=i)
+    values(nssf.low[[i]]) <- ifelse(values(nssf.low[[i]]) < 0.5, 0, 1)
+    nssf.high[[i]] <- raster("data/High RF scenario.tif", band=i)
+    values(nssf.high[[i]]) <- ifelse(values(nssf.high[[i]]) < 0.5, 0, 1)
+}
+
+#par(mfrow=c(4,6), mar=c(1.5,1,2,0))
+#for(i in 1:22) plot(nssf.usual[[i]], main=2020+i, legend=F)
+#par(mfrow=c(4,6), mar=c(1.5,1,2,0))
+#for(i in 1:22) plot(nssf.extreme[[i]], main=2020+i, legend=F)
+#par(mfrow=c(4,6), mar=c(1.5,1,2,0))
+#for(i in 1:22) plot(nssf.low[[i]], main=2020+i, legend=F)
+#par(mfrow=c(4,6), mar=c(1.5,1,2,0))
+#for(i in 1:22) plot(nssf.high[[i]], main=2020+i, legend=F)
+#par(mfrow=c(2,2), mar=c(1.5,1,2,0))
+#plot(nssf.usual[[22]], main="As usual", legend=F)
+#plot(nssf.extreme[[22]], main="Extreme drying", legend=F)
+#plot(nssf.low[[22]], main="Low rainfall", legend=F)
+#plot(nssf.high[[22]], main="High rainfall", legend=F)
+
+##################################
+# Estimate baseline stem numbers #
+#  for initializing populations  #
+##################################
+
+# read in survey stem densities of the four focal spp
+distrib <- read.csv("data/Size distribution.csv")
+
+# Among trees (>5cm DBH), the summed BA in swamp/non-swamp plots for each sp:
+BA.plots <- matrix(c(0, 7373.44, 2408.03, 11833.72, 1147.71, 2552.13), 
+       ncol=2, dimnames=list(c("PPI","PPO", "SCE"), c("non-swamp", "swamp")))
+
+# And the BA relative to total BA across all plots of that type
+BA.plots.prop <- BA.plots / matrix(rep(c(207239.22, 244171.15), each=3), 3, 2)
+
+# proportion of the landscape that is swamp/non-swamp:
+prop.swamp <- sum(values(nssf.usual[[1]]), na.rm=T)/length(na.omit(values(nssf.usual[[1]])))
+#prop.swamp <- sum(values(nssf.extreme[[1]]), na.rm=T)/length(na.omit(values(nssf.extreme[[1]])))
+prop.ns <- 1-prop.swamp
+
+# total no. of stems across NSSF estimated from raw data:
+# trees: 1,020,479
+# saplings: 3,171,812
+# seedlings: 33,424,301
+# total: ~37.6 million
+# seedlings are likely overestimated because seedling quadrats are not randomly placed (maybe 2x overestimated?)
+# therefore true ratio probably closer to 1:3:16, with a total of 20million stems
+n.total <- 20000000
+# but 20 million stems is still a bit too much to estimate
+# introduce a dilution factor
+dilution <- 20
+n.init <- round(BA.plots.prop * matrix(rep(c(prop.ns, prop.swamp), each=3), 3, 2) * n.total/dilution, 0)
+n.init[1,1] <- 100 # cannot have zeroes
+
+
+# Generating random initial locations
+
+# convert baselayer from raster to polygons for random point generation
+swamp.poly <- rasterToPolygons(nssf.usual[[1]], fun=function(x){x==1})
+nonswamp.poly <- rasterToPolygons(nssf.usual[[1]], fun=function(x){x==0})
+#swamp.poly <- rasterToPolygons(nssf.extreme[[1]], fun=function(x){x==1})
+#nonswamp.poly <- rasterToPolygons(nssf.extreme[[1]], fun=function(x){x==0})
+
+# abbreviated species names for for loop
+tran.abbrev <- c("AAN", "AFR", "ALU", "ACL","BBR", "BPA", "CRU", "CSQ", "CBR", 
+                   "EMA", "GPA", "GNE", "GWA", "GAX", "KMA", "MBA", "PAX", "PPI", "PPO", 
+                   "PCO", "PEC", "SCE", "SPA", "TFL", "TWA", "XFL", "AOS")
+for(i in 1:3){
+    # abbreviated sp name
+    sp <- rownames(n.init)[i]
+    
+    # breakdown population into adults (>5cm DBH), saplings (5>DBH>1cm) and seedlings (<1cm DBH) and swamp/non-swamp
+    # 1/20, 3/20, 16/20 are proportions of each demographic group to total number of stems
+    n.swamp.adults <- round(1/20 * n.init[i,"swamp"], 0)
+    n.swamp.saplings <- round(3/20 * n.init[i,"swamp"], 0)
+    n.swamp.seedlings <- n.init[i,"swamp"] - n.swamp.adults - n.swamp.saplings
+    n.nonswamp.adults <- round(1/20 * n.init[i,"non-swamp"], 0)
+    n.nonswamp.saplings <- round(3/20 * n.init[i,"non-swamp"], 0)
+    n.nonswamp.seedlings <- n.init[i,"non-swamp"] - n.nonswamp.adults - n.nonswamp.saplings
+    
+    stopifnot(
+        "Some populations have zero count! spsample wil fail. Check if dilution is too high?" = all(
+            n.swamp.adults > 0,
+            n.swamp.saplings > 0,
+            n.swamp.seedlings > 0,
+            n.nonswamp.adults > 0,
+            n.nonswamp.saplings > 0,
+            n.nonswamp.seedlings > 0
+        )
+    )
+    
+    # sample swamp/non-swamp areas according to adults (adults + saplings) and seedling groups
+    swamp.pts.T <- spsample(swamp.poly, n.swamp.adults+n.swamp.saplings, type = 'random')
+    nonswamp.pts.T <- spsample(nonswamp.poly, n.nonswamp.adults+n.nonswamp.saplings, type = 'random')
+    swamp.pts.S <- spsample(nonswamp.poly, n.swamp.seedlings, type = 'random')
+    nonswamp.pts.S <- spsample(nonswamp.poly, n.nonswamp.seedlings, type = 'random')
+    
+    # combine random coordinates with sizes drawn from raw data
+    assign(paste0(tolower(sp),"T"), rbind(
+        data.frame(coordinates(swamp.pts.T), 
+              logdbh=log(c(
+                  sample(distrib$dbh[which(distrib$species==sp & distrib$cat=="tree")], size=n.swamp.adults, replace=T),
+                  sample(distrib$dbh[which(distrib$species==sp & distrib$cat=="sapling")], size=n.swamp.saplings, replace=T)
+                  ))
+              ),
+        data.frame(coordinates(nonswamp.pts.T),
+              logdbh=log(c(
+                  sample(distrib$dbh[which(distrib$species==sp & distrib$cat=="tree")], size=n.nonswamp.adults, replace=T),
+                  sample(distrib$dbh[which(distrib$species==sp & distrib$cat=="sapling")], size=n.nonswamp.saplings, replace=T)
+                  ))
+              )
+        ))
+    
+    # for seedlings, draw from raw data first, transform (DBH to height) later
+    z <- log(c(
+        sample(distrib$dbh[which(distrib$species==sp & distrib$cat=="seedling")], size=n.swamp.seedlings, replace=T),
+        sample(distrib$dbh[which(distrib$species==sp & distrib$cat=="seedling")], size=n.nonswamp.seedlings, replace=T)
+        ))
+    
+    # combine random coordinates with sizes drawn from raw data
+    assign(paste0(tolower(sp),"S"),rbind(
+        data.frame(
+            rbind(coordinates(swamp.pts.S), coordinates(nonswamp.pts.S)),
+            # transformation of logDBH to logheight
+            logheight = (z - tran.parm["tran.int",which(tran.abbrev==sp)]) / tran.parm["tran.h",which(tran.abbrev==sp)]
+        )
+    ))
+    
+}
+
+#par(mfrow=c(1,3)); hist(ppoT[,"logdbh"], main="PPO"); hist(sceT[,"logdbh"], main="SCE"); hist(ppiT[,"logdbh"], main="PPI")
+
+## initialize "All other species" for background interspecific competition
+
+# Approx. number of all other species stems (ADULTS ONLY--using 1:3:20 adults to saplings to seedlings ratio)
+init.aos.n <- round(4/20 * (n.total/dilution - sum(n.init)), 0)
+
+# Use power law with 1cm min DBH for all other spp. Note that seedlings not modelled
+init.aos.dbh <- rplcon(init.aos.n, 1, 2)
+aosT.logdbh <- log(init.aos.dbh)
+#hist(aosT.logdbh)
+
+# create random points for aosT, assign them logdbh values
+NSSFpolygon <- readOGR("data/NSSF2_Catchment.shp")
+aos.loc <- spsample(NSSFpolygon, init.aos.n, type = 'random')
+aosT <- data.frame(coordinates(aos.loc), logdbh = aosT.logdbh)
+
+###################################
+# Set up grid for reducing memory #
+#   consumption when computing    #
+#   inter-individual distances    #
+###################################
+
+# create low resolution raster (100 x 100m)
+nssf.100 <- aggregate(nssf.extreme[[1]], fact=5)
+res(nssf.100)
+# assign cell values = index
+nssf.100 <- setValues(nssf.100, 1:length(nssf.100))
+# create list of indexed grid neighbours (NOT including focal grid cell)
+grid.neighbours <- list()
+for(i in 1:length(nssf.100)) grid.neighbours[[i]] <- adjacent(nssf.100, i, directions=8, matrix=F)[,2]
+
+# Add another attribute (column) to individual data
+ppoT$grid <- extract(nssf.100, ppoT[,c("x","y")])
+ppoS$grid <- extract(nssf.100, ppoS[,c("x","y")])
+
+sceT$grid <- extract(nssf.100, sceT[,c("x","y")])
+sceS$grid <- extract(nssf.100, sceS[,c("x","y")])
+
+ppiT$grid <- extract(nssf.100, ppiT[,c("x","y")])
+ppiS$grid <- extract(nssf.100, ppiS[,c("x","y")])
+
+aosT$grid <- extract(nssf.100, aosT[,c("x","y")])
+
+#############
+# Visualize #
+#############
+
+# # Define a colour palette
+# col.pal <- c("#E3C16F", "#946846", "#FAFF70", "#6D213C", "#65DEF1", "#F3F5F6", "#BAAB68", "#CBC5EA")
+# col.t <- adjustcolor(col.pal, alpha.f=0.6)
+# 
+# plot(nssf.usual[[1]], legend=F, col=col.pal[c(6,5)])
+# # PPO
+# points(ppoT, cex=ppoT$logdbh/2, col=col.t[2], pch=16)
+# #points(data.frame(ppoS), col=col.t[1], pch=4, cex=0.1)
+# # SCE
+# points(sceT, cex=sceT$logdbh/2, col=col.t[1], pch=16)
+# #points(data.frame(sceS), col=col.t[2], pch=4, cex=0.1)
+# # PPI
+# points(ppiT, cex=ppiT$logdbh/2, col=col.t[3], pch=16)
+# #points(data.frame(ppiS), col=col.t[3], pch=4, cex=0.1)
+# # GNE
+# points(gneT, cex=gneT$logdbh/2, col=col.t[3], pch=16)
+# #points(data.frame(gneS), col=col.t[4], pch=4, cex=0.1)
+# # All other species (static--just for a background interspecific competition pressure)
+# #points(aosT, cex=aosT$logdbh/5,  pch=1, col="grey70")
+# scalebar(500, xy=c(368500, 151650), type="bar", lonlat=F, below="metres", divs=2)
---
+++ code/three_spp_IBM.R
@@ -0,0 +1,319 @@
+library(raster)
+library(sp)
+library(poweRlaw)
+library(doParallel)
+library(rgdal)
+
+# Settings for parallerisation
+registerDoParallel(cores = 64)
+
+
+#################
+# GROWTH PARAMS #
+#################
+
+grow.T.parm <- t(read.csv("data/tree growth parameters Apr21.csv", header=T, row.names=1))
+grow.T.parm.unscale <- read.csv("data/tree growth model unscale params.csv", header=T, row.names=1)
+grow.S.parm <- read.csv("data/seedling lmer growth params CAA Apr21.csv", header=T, row.names=1)
+surv.parm <- t(read.csv("data/surv params Jun21.csv", header=T, row.names=1))
+surv.parm.unscale <- read.csv("data/survival model unscale params.csv", header=T, row.names=1)
+fruit.parm <- read.csv("data/fruiting parameters Apr21.csv", header=T, row.names=1)
+tran.parm <- read.csv("data/transition params CAA Jun21.csv", header=T, row.names=1)
+rec.parm <- t(read.csv("data/dispersal kernel parameters Apr21.csv", header=T, row.names=1))
+HM.parm <- t(read.csv("data/HM values.csv", header=T, row.names=1))
+
+# standardize some spp names
+colnames(surv.parm) <- gsub(" ", ".", colnames(surv.parm))
+colnames(grow.T.parm) <- gsub(" ", ".", colnames(grow.T.parm))
+colnames(HM.parm) <- gsub(" ", ".", colnames(HM.parm))
+
+# add a "min size" row to rec.parm
+rec.parm <- rbind(rec.parm, rec.parm["rcsz",] - 2*rec.parm["rcsd",])
+rownames(rec.parm)[5] <- "minsize"
+
+##################
+# LOAD FUNCTIONS #
+##################
+source("code/functions.R")
+
+
+#################
+# SETUP BASEMAP #
+#################
+source("code/setup_map_three spp.R")
+
+
+#######################
+# COMMENCE SIMULATION #
+#######################
+
+time <- 1
+maxTime <- 23
+
+# take a snapshot of initial conditions
+ppoT.init <- ppoT
+ppoS.init <- ppoS
+sceT.init <- sceT
+sceS.init <- sceS
+ppiT.init <- ppiT
+ppiS.init <- ppiS
+
+# record population sizes
+n.ppoT <- nrow(ppoT)
+n.ppoS <- nrow(ppoS)
+n.sceT <- nrow(sceT)
+n.sceS <- nrow(sceS)
+n.ppiT <- nrow(ppiT)
+n.ppiS <- nrow(ppiS)
+
+# record mean sizes
+z.ppoT <- mean(ppoT$logdbh)
+h.ppoS <- mean(ppoS$logheight)
+z.sceT <- mean(sceT$logdbh)
+h.sceS <- mean(sceS$logheight)
+z.ppiT <- mean(ppiT$logdbh)
+h.ppiS <- mean(ppiS$logheight)
+
+# create table for total BA in swamp vs non-swamp
+ba.ppoT <- data.frame(time=seq(time, maxTime, 1), nonswamp=NA, swamp=NA, landscape.swamp.prop=NA)
+ba.sceT <- data.frame(time=seq(time, maxTime, 1), nonswamp=NA, swamp=NA, landscape.swamp.prop=NA)
+ba.ppiT <- data.frame(time=seq(time, maxTime, 1), nonswamp=NA, swamp=NA, landscape.swamp.prop=NA)
+
+# progress bar
+pb <- txtProgressBar(min = 1, max = maxTime, style = 3)
+while (time <= maxTime) {
+  
+  # Choose landscape based on scenario and time (beyond 22 years, just use the last time point (=2042))
+  nssf.m <- nssf.extreme[[ifelse(time>22, 22, time)]]
+  
+  # Recruitment: initiate fruiting, but don't add new recruits to seedling dfs yet (let them grow/die first)
+  # PPO ("Prunus.polystachya")
+  fruiting.index.ppo <- which(p_bz(nssf.m, ppoT, "Prunus.polystachya")==1)
+  prod.vec.ppo <- b_z(nssf.m, ppoT[fruiting.index.ppo,], "Prunus.polystachya")
+  parent.loc.ppo <- ppoT[fruiting.index.ppo, 1:2]
+  # SCE ("Strombosia.ceylanica")
+  fruiting.index.sce <- which(p_bz(nssf.m, sceT, "Strombosia.ceylanica")==1)
+  prod.vec.sce <- b_z(nssf.m, sceT[fruiting.index.sce,], "Strombosia.ceylanica")
+  parent.loc.sce <- sceT[fruiting.index.sce, 1:2]
+  # PPI ("Pometia pinnata")
+  fruiting.index.ppi <- which(p_bz(nssf.m, ppiT, "Pometia.pinnata")==1)
+  prod.vec.ppi <- b_z(nssf.m, ppiT[fruiting.index.ppi,], "Pometia.pinnata")
+  parent.loc.ppi <- ppiT[fruiting.index.ppi, 1:2]
+  
+  # Extract competition measures
+  # PPO ("Prunus.polystachya")
+  inter.on.PPO <- inter.calc(rbind(sceT, ppiT, aosT), rbind(sceS, ppiS), ppoT, ppoS, 
+                             sp="Prunus.polystachya", grid.neighbours = grid.neighbours)
+  intra.on.PPO <- intra.calc(ppoT, ppoS, sp="Prunus.polystachya", grid.neighbours = grid.neighbours)
+  CAE.on.ppoS <- CAE.calc(ppoT, ppoS, grid.neighbours = grid.neighbours)
+  HAE.on.ppoS <- HAE.calc(rbind(sceT, ppiT, aosT), ppoS, grid.neighbours = grid.neighbours)
+  intra.dist.on.PPO <- intra.dist.calc(ppoT, grid.neighbours = grid.neighbours)
+  inter.dist.on.PPO <- inter.dist.calc(rbind(sceT, ppiT, aosT), ppoT, grid.neighbours = grid.neighbours)
+  # SCE ("Strombosia.ceylanica")
+  inter.on.SCE <- inter.calc(rbind(ppoT, ppiT, aosT), rbind(ppoS, ppiS), sceT, sceS, 
+                             sp="Strombosia.ceylanica", grid.neighbours = grid.neighbours)
+  intra.on.SCE <- intra.calc(sceT, sceS, sp="Strombosia.ceylanica", grid.neighbours = grid.neighbours)
+  CAE.on.sceS <- CAE.calc(sceT, sceS, grid.neighbours = grid.neighbours)
+  HAE.on.sceS <- HAE.calc(rbind(ppoT, ppiT, aosT), sceS, grid.neighbours = grid.neighbours)
+  intra.dist.on.SCE <- intra.dist.calc(sceT, grid.neighbours = grid.neighbours)
+  inter.dist.on.SCE <- inter.dist.calc(rbind(ppoT, ppiT, aosT), sceT, grid.neighbours = grid.neighbours)
+  # PPI ("Pometia.pinnata")
+  inter.on.PPI <- inter.calc(rbind(sceT, ppoT, aosT), rbind(sceS, ppoS), ppiT, ppiS, 
+                             sp="Pometia.pinnata", grid.neighbours = grid.neighbours)
+  intra.on.PPI <- intra.calc(ppiT, ppiS, sp="Pometia.pinnata", grid.neighbours = grid.neighbours)
+  CAE.on.ppiS <- CAE.calc(ppiT, ppiS, grid.neighbours = grid.neighbours)
+  HAE.on.ppiS <- HAE.calc(rbind(sceT, ppoT, aosT), ppiS, grid.neighbours = grid.neighbours)
+  intra.dist.on.PPI <- intra.dist.calc(ppiT, grid.neighbours = grid.neighbours)
+  inter.dist.on.PPI <- inter.dist.calc(rbind(sceT, ppoT, aosT), ppiT, grid.neighbours = grid.neighbours)
+  
+  # Seedling growth and survival
+  # PPO ("Prunus.polystachya")
+  ppoS$logheight <- sS_h(nssf.m, ppoS, "Prunus.polystachya", intra.on.PPO[[4]], inter.on.PPO[[4]]) * 
+    GS_h1h(nssf.m, ppoS, "Prunus.polystachya", CAE.on.ppoS, HAE.on.ppoS)
+  if(sum(ppoS$logheight < rec.parm["minsize", "Prunus.polystachya"], na.rm = TRUE) > 0){
+    ppoS <- ppoS[-which(ppoS$logheight < rec.parm["minsize", "Prunus.polystachya"]),] }
+  # SCE ("Strombosia.ceylanica")
+  sceS$logheight <- sS_h(nssf.m, sceS, "Strombosia.ceylanica", intra.on.SCE[[4]], inter.on.SCE[[4]]) * 
+    GS_h1h(nssf.m, sceS, "Strombosia.ceylanica", CAE.on.sceS, HAE.on.sceS)
+  if(sum(sceS$logheight < rec.parm["minsize", "Strombosia.ceylanica"], na.rm = TRUE) > 0){
+    sceS <- sceS[-which(sceS$logheight < rec.parm["minsize", "Strombosia.ceylanica"]),] }
+  # PPI ("Pometia.pinnata")
+  ppiS$logheight <- sS_h(nssf.m, ppiS, "Pometia.pinnata", intra.on.PPI[[4]], inter.on.PPI[[4]]) * 
+    GS_h1h(nssf.m, ppiS, "Pometia.pinnata", CAE.on.ppiS, HAE.on.ppiS)
+  if(sum(ppiS$logheight < rec.parm["minsize", "Pometia.pinnata"], na.rm = TRUE) > 0){
+    ppiS <- ppiS[-which(ppiS$logheight < rec.parm["minsize", "Pometia.pinnata"]),] }
+  
+  # Tree growth and survival
+  # PPO ("Prunus.polystachya")
+  ppoT$logdbh <- sT_z(nssf.m, ppoT, "Prunus.polystachya", intra.on.PPO[[1]], intra.on.PPO[[3]], inter.on.PPO[[1]], inter.on.PPO[[3]]) * 
+    GT_z1z(nssf.m, ppoT, "Prunus.polystachya", intra.dist.on.PPO, inter.dist.on.PPO)
+  if(sum(ppoT$logdbh==0, na.rm = TRUE) > 0)  ppoT <- ppoT[-which(ppoT$logdbh==0),]
+  # SCE ("Strombosia.ceylanica")
+  sceT$logdbh <- sT_z(nssf.m, sceT, "Strombosia.ceylanica", intra.on.SCE[[1]], intra.on.SCE[[3]], inter.on.SCE[[1]], inter.on.SCE[[3]]) * 
+    GT_z1z(nssf.m, sceT, "Strombosia.ceylanica", intra.dist.on.SCE, inter.dist.on.SCE)
+  if(sum(sceT$logdbh==0, na.rm = TRUE) > 0)  sceT <- sceT[-which(sceT$logdbh==0),]
+  # PPI ("Pometia.pinnata")
+  ppiT$logdbh <- sT_z(nssf.m, ppiT, "Pometia.pinnata", intra.on.PPI[[1]], intra.on.PPI[[3]], inter.on.PPI[[1]], inter.on.PPI[[3]]) * 
+    GT_z1z(nssf.m, ppiT, "Pometia.pinnata", intra.dist.on.PPI, inter.dist.on.PPI)
+  if(sum(ppiT$logdbh==0, na.rm = TRUE) > 0)  ppiT <- ppiT[-which(ppiT$logdbh==0),]
+  
+  # Conserve memory by deleting competition indices (don't need anymore)
+  rm(inter.on.PPO, intra.on.PPO, CAE.on.ppoS, HAE.on.ppoS, intra.dist.on.PPO, inter.dist.on.PPO,
+     inter.on.SCE, intra.on.SCE, CAE.on.sceS, HAE.on.sceS, intra.dist.on.SCE, inter.dist.on.SCE,
+     inter.on.PPI, intra.on.PPI, CAE.on.ppiS, HAE.on.ppiS, intra.dist.on.PPI, inter.dist.on.PPI)
+  
+  # Seedling-sapling transition
+  # PPO ("Prunus.polystachya")
+  ppoTS <- T_z1h1(ppoT, ppoS, "Prunus.polystachya")
+  ppoT <- ppoTS[[1]]
+  ppoS <- ppoTS[[2]]
+  rm(ppoTS)
+  # SCE ("Strombosia.ceylanica")
+  sceTS <- T_z1h1(sceT, sceS, "Strombosia.ceylanica")
+  sceT <- sceTS[[1]]
+  sceS <- sceTS[[2]]
+  rm(sceTS)
+  # PPI ("Pometia.pinnata")
+  ppiTS <- T_z1h1(ppiT, ppiS, "Pometia.pinnata")
+  ppiT <- ppiTS[[1]]
+  ppiS <- ppiTS[[2]]
+  rm(ppiTS)
+  
+  # This is the current number of seedlings:
+  n.old.ppoS <- nrow(ppoS)
+  n.old.sceS <- nrow(sceS)
+  n.old.ppiS <- nrow(ppiS)
+  
+  # Recruitment: now add new recruits to ppoS
+  # PPO ("Prunus.polystachya")
+  if(length(prod.vec.ppo) > 0){
+    for(i in 1:length(prod.vec.ppo)){
+      distances <- twoDT.sample(prod.vec.ppo[i], "Prunus.polystachya", max_dist = 410)
+      angles <- runif(length(distances), min=0, max=360)
+      x <- parent.loc.ppo[i,1] + (distances * cos(angles))
+      y <- parent.loc.ppo[i,2] + (distances * sin(angles))
+      grid <- extract(nssf.100, cbind(x,y))
+      logheight <- c_0h1(length(distances), "Prunus.polystachya")
+      recruits <- cbind(x, y, logheight, grid)
+      # use overlap function to filter out recruits which fail because they land on spots already occupied by trees/seedlings
+      if (nrow(recruits) > 0) {
+        recruits.success <- rm.overlap(recruits=recruits, 
+                                       trees=rbind(ppoT, sceT, ppiT, aosT), 
+                                       seedlings=rbind(ppoS, sceS, ppiS),
+                                       grid.neighbours=grid.neighbours)
+        # if there are still successfully recruiting seedlings, rbind them to existing seedlings
+        if(nrow(recruits.success) > 0) ppoS <- rbind(ppoS, recruits.success)
+      }
+    }
+  }
+  # SCE ("Strombosia.ceylanica")
+  if(length(prod.vec.sce) > 0){
+    for(i in 1:length(prod.vec.sce)){
+      distances <- twoDT.sample(prod.vec.sce[i], "Strombosia.ceylanica", max_dist = 410)
+      angles <- runif(length(distances), min=0, max=360)
+      x <- parent.loc.sce[i,1] + (distances * cos(angles))
+      y <- parent.loc.sce[i,2] + (distances * sin(angles))
+      grid <- extract(nssf.100, cbind(x,y))
+      logheight <- c_0h1(length(distances), "Strombosia.ceylanica")
+      recruits <- cbind(x, y, logheight, grid)
+      if (nrow(recruits) > 0) {
+        recruits.success <- rm.overlap(recruits, 
+                                       trees=rbind(ppoT, sceT, ppiT, aosT), 
+                                       seedlings=rbind(ppoS, sceS, ppiS),
+                                       grid.neighbours=grid.neighbours)
+        if(nrow(recruits.success) > 0) sceS <- rbind(sceS, recruits.success)
+      }
+    }
+  }
+  # PPI ("Pometia.pinnata")
+  if(length(prod.vec.ppi) > 0){
+    for(i in 1:length(prod.vec.ppi)){
+      distances <- twoDT.sample(prod.vec.ppi[i], "Pometia.pinnata", max_dist = 410)
+      angles <- runif(length(distances), min=0, max=360)
+      x <- parent.loc.ppi[i,1] + (distances * cos(angles))
+      y <- parent.loc.ppi[i,2] + (distances * sin(angles))
+      grid <- extract(nssf.100, cbind(x,y))
+      logheight <- c_0h1(length(distances), "Pometia.pinnata")
+      recruits <- cbind(x, y, logheight, grid)
+      # use overlap function to filter out recruits which fail because they land on spots already occupied by trees/seedlings
+      if (nrow(recruits) > 0) {
+        recruits.success <- rm.overlap(recruits=recruits, 
+                                       trees=rbind(ppoT, sceT, ppiT, aosT), 
+                                       seedlings=rbind(ppoS, sceS, ppiS),
+                                       grid.neighbours=grid.neighbours)
+        # if there are still successfully recruiting seedlings, rbind them to existing seedlings
+        if(nrow(recruits.success) > 0) ppiS <- rbind(ppiS, recruits.success)
+      }
+    }
+  }
+  
+  # Kill off 50% of all recruits that are located < 10cm from each other
+  # temporarily omitted
+  #ppoS <- kill.rec(ppoS, n.old.ppoS)
+  #sceS <- kill.rec(sceS, n.old.sceS)
+  #ppiS <- kill.rec(ppiS, n.old.ppiS)
+  
+  # Take stock of all individuals
+  # PPO ("Prunus.polystachya")
+  n1.ppoT <- nrow(ppoT)
+  n.ppoT <- c(n.ppoT, n1.ppoT) 
+  n1.ppoS <- nrow(ppoS)
+  n.ppoS <- c(n.ppoS, n1.ppoS) 
+  # SCE ("Strombosia.ceylanica")
+  n1.sceT <- nrow(sceT)
+  n.sceT <- c(n.sceT, n1.sceT) 
+  n1.sceS <- nrow(sceS)
+  n.sceS <- c(n.sceS, n1.sceS) 
+  # PPI ("Pometia.pinnata")
+  n1.ppiT <- nrow(ppiT)
+  n.ppiT <- c(n.ppiT, n1.ppiT) 
+  n1.ppiS <- nrow(ppiS)
+  n.ppiS <- c(n.ppiS, n1.ppiS) 
+  
+  # calculate mean sizes of adults and saplings and add to string  
+  # PPO ("Prunus.polystachya")
+  z.ppoT <- c(z.ppoT, mean(ppoT$logdbh))
+  h.ppoS <- c(h.ppoS, mean(ppoS$logheight))
+  # SCE ("Strombosia.ceylanica")
+  z.sceT <- c(z.sceT, mean(sceT$logdbh))
+  h.sceS <- c(h.sceS, mean(sceS$logheight))
+  # PPI ("Pometia.pinnata")
+  z.ppiT <- c(z.ppiT, mean(ppiT$logdbh))
+  h.ppiS <- c(h.ppiS, mean(ppiS$logheight))
+  
+  # record total basal areas of adults in swamp versus non-swamp areas
+  ba.ppoT[which(ba.ppoT$time==time), 2:3] <- tapply(pi*(exp(ppoT[,"logdbh"])/2)^2, extract(nssf.m, ppoT[,1:2]), sum)
+  ba.sceT[which(ba.sceT$time==time), 2:3] <- tapply(pi*(exp(sceT[,"logdbh"])/2)^2, extract(nssf.m, sceT[,1:2]), sum)
+  ba.ppiT[which(ba.ppiT$time==time), 2:3] <- tapply(pi*(exp(ppiT[,"logdbh"])/2)^2, extract(nssf.m, ppiT[,1:2]), sum)
+  # to calculate SSI need to know area of swamp versus non-swamp across landscape at that time point
+  ba.ppoT[which(ba.ppoT$time==time), "landscape.swamp.prop"] <- sum(values(nssf.m), na.rm = T) / length(na.omit(values(nssf.m)))
+  ba.sceT[which(ba.sceT$time==time), "landscape.swamp.prop"] <- sum(values(nssf.m), na.rm = T) / length(na.omit(values(nssf.m)))
+  ba.ppiT[which(ba.ppiT$time==time), "landscape.swamp.prop"] <- sum(values(nssf.m), na.rm = T) / length(na.omit(values(nssf.m)))
+
+  # print message to check status during runs
+  message(paste(n1.ppoT + n1.ppoS + n1.sceT + n1.sceS + n1.ppiT + n1.ppiS, "stems at year", time, "  "),
+          appendLF = TRUE)
+    
+  # move to next time point
+  time <- time + 1
+  # update progress bar
+  setTxtProgressBar(pb, time)
+}
+
+close(pb)
+
+# output to be saved
+out3.usual <- 
+  list(nssf.m = nssf.m, 
+       ppoT.init = ppoT.init, sceT.init = sceT.init, ppiT.init = ppiT.init, 
+       ppoS.init = ppoS.init, sceS.init = sceS.init, ppiS.init = ppiS.init,
+       ppoT = ppoT, sceT = sceT, ppiT = ppiT,
+       ppoS = ppoS, sceS = sceS, ppiS = ppiS,
+       n.ppoT = n.ppoT, n.sceT = n.sceT, n.ppiT = n.ppiT,
+       n.ppoS = n.ppoS, n.sceS = n.sceS, n.ppiS = n.ppiS,
+       z.ppoT = z.ppoT, z.sceT = z.sceT, z.ppiT = z.ppiT,
+       h.ppoS = h.ppoS, h.sceS = h.sceS, h.ppiS = h.ppiS,
+       ba.ppoT = ba.ppoT, ba.sceT = ba.sceT, ba.ppiT = ba.ppiT)
+saveRDS(out3.usual, file = "out/sim_out3_usual.rds")
+
+
