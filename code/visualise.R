0ce017c6a4fb9b4704092534fd813fbbe55fef6c
Weng Ngai Lam <wengngai@u.nus.edu>
2021-08-04 11:05:17 +0800

    Modified for 3 spp model

--- code/visualise.R
+++ code/visualise.R
@@ -2,7 +2,7 @@
 # PLOT OUTPUTS  #
 #################
 
-usual <- readRDS(file = "out/sim_out.rds")
+usual <- readRDS(file = "out/sim_out3_usual.rds")
 summary(usual)
 
 # Define a colour palette
@@ -30,12 +30,10 @@ for(i in 1:22){
 
 
 # Plot out 10% of population
-#pdf(file = "D:\\Dropbox\\twn idiwn\\Post doc\\IBM temp\\distributions Jul21.pdf", width=16, height=9)
+#pdf(file = "D:\\Dropbox\\twn idiwn\\Post doc\\IBM temp\\distributions 3spp Aug21.pdf", width=16, height=9)
 # Initial condition
 par(mfrow=c(1,2), mar=c(5,5,3,2))
 plot(nssf.usual[[1]], legend=F, col=col.pal[c(6,5)], main="2021")
-# GNE
-points(usual$gneT.init[sample(nrow(usual$gneT.init), size=nrow(usual$gneT.init)/10),], cex=usual$gneT.init$logdbh/2, col=col.t[4], pch=16)
 # PPO
 points(usual$ppoT.init[sample(nrow(usual$ppoT.init), size=nrow(usual$ppoT.init)/10),], cex=usual$ppoT.init$logdbh/2, col=col.t[1], pch=16)
 # SCE
@@ -44,9 +42,8 @@ points(usual$sceT.init[sample(nrow(usual$sceT.init), size=nrow(usual$sceT.init)/
 points(usual$ppiT.init[sample(nrow(usual$ppiT.init), size=nrow(usual$ppiT.init)/10),], cex=usual$ppiT.init$logdbh/2, col=col.t[3], pch=16)
 # All other species (static--just for a background interspecific competition pressure)
 #points(aosT, cex=aosT$logdbh/2,  pch=1, col="grey70")
-legend('topleft', col=col.t[c(4,1,2,3)], pch=16, bty="n", pt.cex=2,
+legend('topleft', col=col.t[c(1,2,3)], pch=16, bty="n", pt.cex=2,
        legend=c(
-               "G. nervosa (non-swamp specialist)",
                "P. polystachya (non-swamp generalist)",
                "S. ceylanica (swamp generalist)",
                "P. pinnata (swamp specialist)"
@@ -54,8 +51,6 @@ legend('topleft', col=col.t[c(4,1,2,3)], pch=16, bty="n", pt.cex=2,
 
 # Final condition
 plot(nssf.usual[[22]], legend=F, col=col.pal[c(6,5)], main="2042")
-# GNE
-points(usual$gneT[sample(nrow(usual$gneT), size=nrow(usual$gneT)/10),], cex=usual$gneT$logdbh/2, col=col.t[4], pch=16)
 # PPO
 points(usual$ppoT[sample(nrow(usual$ppoT), size=nrow(usual$ppoT)/10),], cex=usual$ppoT$logdbh/2, col=col.t[1], pch=16)
 # SCE
@@ -67,106 +62,87 @@ points(usual$ppiT[sample(nrow(usual$ppiT), size=nrow(usual$ppiT)/10),], cex=usua
 scalebar(400, xy=c(367800, 151550), type="bar", lonlat=F, below="metres", divs=2)
 dev.off()
 
-#pdf(file = "D:\\Dropbox\\twn idiwn\\Post doc\\IBM temp\\populations Jul21.pdf", width=16, height=9)
+#pdf(file = "D:\\Dropbox\\twn idiwn\\Post doc\\IBM temp\\populations 3spp Aug21.pdf", width=16, height=9)
 par(mfrow=c(1,2), mar=c(5.5,5.5,2,2))
 plot(log(usual$n.ppoT) ~ c(1:length(usual$n.ppoT)), lwd=5, col=col.pal[2], type="l",
      ylab="Adult population size (log-transformed)", xlab="Time (years)", cex.lab=1.5, cex.axis=1.5,
-     ylim=log(c(min(c(usual$n.ppoT, usual$n.sceT, usual$n.ppiT, usual$n.gneT)), 
-            max(c(usual$n.ppoT, usual$n.sceT, usual$n.ppiT, usual$n.gneT))))
+     ylim=log(c(min(c(usual$n.ppoT, usual$n.sceT, usual$n.ppiT)), 
+            max(c(usual$n.ppoT, usual$n.sceT, usual$n.ppiT))))
      )
 lines(log(usual$n.sceT) ~ c(1:length(usual$n.sceT)), lwd=5, col=col.pal[1])
-lines(log(usual$n.gneT) ~ c(1:length(usual$n.gneT)), lwd=5, col=col.pal[4])
 lines(log(usual$n.ppiT) ~ c(1:length(usual$n.ppiT)), lwd=5, col=col.pal[3])
-legend('topright', col=col.pal[c(4,2,1,3)], bty="n", lwd=3, lty=1, title="Species",
+legend('topleft', col=col.pal[c(2,1,3)], bty="n", lwd=3, lty=1, title="Species",
        legend=c(
-               "G. nervosa (non-swamp specialist)",
                "P. polystachya (non-swamp generalist)",
                "S. ceylanica (swamp generalist)",
                "P. pinnata (swamp specialist)"
        ))
 plot(log(usual$n.ppoS) ~ c(1:length(usual$n.ppoS)), lwd=5, col=col.pal[2], type="l",
      ylab="Seedling population size (log-transformed)", xlab="Time (years)", cex.lab=1.5, cex.axis=1.5,
-     ylim=log(c(min(c(usual$n.ppoS, usual$n.sceS, usual$n.ppiS, usual$n.gneS)), 
-            max(c(usual$n.ppoS, usual$n.sceS, usual$n.ppiS, usual$n.gneS))))
+     ylim=log(c(min(c(usual$n.ppoS, usual$n.sceS, usual$n.ppiS)), 
+            max(c(usual$n.ppoS, usual$n.sceS, usual$n.ppiS))))
      )
 lines(log(usual$n.sceS) ~ c(1:length(usual$n.sceS)), lwd=5, col=col.pal[1])
 lines(log(usual$n.ppiS) ~ c(1:length(usual$n.ppiS)), lwd=5, col=col.pal[3])
-lines(log(usual$n.gneS) ~ c(1:length(usual$n.gneS)), lwd=5, col=col.pal[4])
 dev.off()
 
-#pdf(file = "D:\\Dropbox\\twn idiwn\\Post doc\\IBM temp\\sizes Jul21.pdf", width=16, height=9)
+#pdf(file = "D:\\Dropbox\\twn idiwn\\Post doc\\IBM temp\\sizes 3spp Aug21.pdf", width=16, height=9)
 par(mfrow=c(1,2), mar=c(5.5,5.5,2,2))
 plot(usual$z.ppoT ~ c(1:length(usual$z.ppoT)), lwd=5, col=col.pal[2], type="l",
      ylab="Mean adult DBH (log-transformed)", xlab="Time (years)", cex.lab=1.5, cex.axis=1.5,
-     ylim=c(min(c(usual$z.ppoT, usual$z.sceT, usual$z.ppiT, usual$z.gneT)), 
-            max(c(usual$z.ppoT, usual$z.sceT, usual$z.ppiT, usual$z.gneT))))
+     ylim=c(min(c(usual$z.ppoT, usual$z.sceT, usual$z.ppiT)), 
+            max(c(usual$z.ppoT, usual$z.sceT, usual$z.ppiT))))
 lines(usual$z.sceT ~ c(1:length(usual$z.sceT)), lwd=5, col=col.pal[1])
 lines(usual$z.ppiT ~ c(1:length(usual$z.ppiT)), lwd=5, col=col.pal[3])
-lines(usual$z.gneT ~ c(1:length(usual$z.gneT)), lwd=5, col=col.pal[4])
-legend('topleft', col=col.pal[c(4,2,1,3)], bty="n", lwd=3, lty=1, title="Species",
+legend('topleft', col=col.pal[c(2,1,3)], bty="n", lwd=3, lty=1, title="Species",
        legend=c(
-               "G. nervosa (non-swamp specialist)",
                "P. polystachya (non-swamp generalist)",
                "S. ceylanica (swamp generalist)",
                "P. pinnata (swamp specialist)"
        ))
 plot(usual$h.ppoS ~ c(1:length(usual$h.ppoS)), lwd=5, col=col.pal[2], type="l",
      ylab="Mean seedling height (log-transformed)", xlab="Time (years)", cex.lab=1.5, cex.axis=1.5,
-     ylim=c(min(c(usual$h.ppoS, usual$h.sceS, usual$h.ppiS, usual$h.gneS)), 
-            max(c(usual$h.ppoS, usual$h.sceS, usual$h.ppiS, usual$h.gneS))))
+     ylim=c(min(c(usual$h.ppoS, usual$h.sceS, usual$h.ppiS)), 
+            max(c(usual$h.ppoS, usual$h.sceS, usual$h.ppiS))))
 lines(usual$h.sceS ~ c(1:length(usual$h.sceS)), lwd=5, col=col.pal[1])
 lines(usual$h.ppiS ~ c(1:length(usual$h.ppiS)), lwd=5, col=col.pal[3])
-lines(usual$h.gneS ~ c(1:length(usual$h.gneS)), lwd=5, col=col.pal[4])
 dev.off()
 
 
-# Calculate SSI of all four species. Note that labels of the ba.sppT dataframes were wrongly named
-gne.ssi <- 1-with(usual$ba.gneT, swamp*landscape.swamp.prop / ((swamp*landscape.swamp.prop) + (nonswamp*(1-landscape.swamp.prop))))
-ppo.ssi <- 1-with(usual$ba.ppoT, swamp*landscape.swamp.prop / ((swamp*landscape.swamp.prop) + (nonswamp*(1-landscape.swamp.prop))))
-sce.ssi <- 1-with(usual$ba.sceT, swamp*landscape.swamp.prop / ((swamp*landscape.swamp.prop) + (nonswamp*(1-landscape.swamp.prop))))
-ppi.ssi <- 1-with(usual$ba.ppiT, swamp*landscape.swamp.prop / ((swamp*landscape.swamp.prop) + (nonswamp*(1-landscape.swamp.prop))))
+# Calculate SSI of all four species
+ppo.ssi <- with(usual$ba.ppoT, swamp/landscape.swamp.prop / ((swamp/landscape.swamp.prop) + (nonswamp/(1-landscape.swamp.prop))))
+sce.ssi <- with(usual$ba.sceT, swamp/landscape.swamp.prop / ((swamp/landscape.swamp.prop) + (nonswamp/(1-landscape.swamp.prop))))
+ppi.ssi <- with(usual$ba.ppiT, swamp/landscape.swamp.prop / ((swamp/landscape.swamp.prop) + (nonswamp/(1-landscape.swamp.prop))))
 
-#pdf(file = "D:\\Dropbox\\twn idiwn\\Post doc\\IBM temp\\ssi Jul21.pdf", width=6, height=8)
+#pdf(file = "D:\\Dropbox\\twn idiwn\\Post doc\\IBM temp\\ssi 3spp Aug21.pdf", width=6, height=8)
 par(mfrow=c(1,1), mar=c(5,5,2,2))
-plot(gne.ssi ~ c(1:length(gne.ssi)), lwd=5, col=col.pal[4], type="l",
+plot(ppo.ssi ~ c(1:length(ppo.ssi)), lwd=5, col=col.pal[2], type="l",
      ylab="Swamp specialization index", xlab="Time (years)", cex.lab=1.5, cex.axis=1.5,
      ylim=c(0,1))
 rect(0, 0.5, 25, 1.1, col = adjustcolor(col.t[5], alpha.f = 0.4), border=F)
 abline(h=0.5, lty=2, lwd=3)
-lines(ppo.ssi ~ c(1:length(ppo.ssi)), lwd=5, col=col.pal[2], type="l")
 lines(sce.ssi ~ c(1:length(sce.ssi)), lwd=5, col=col.pal[1], type="l")
 lines(ppi.ssi ~ c(1:length(ppi.ssi)), lwd=5, col=col.pal[3], type="l")
-legend('bottomright', col=col.pal[c(4,2,1,3)], bty="n", lwd=3, lty=1, title="Species",
+legend(x=1, y=0.8, col=col.pal[c(2,1,3)], lwd=3, lty=1, title="Species", bg="white",
        legend=c(
-               "G. nervosa (non-swamp specialist)",
                "P. polystachya (non-swamp generalist)",
                "S. ceylanica (swamp generalist)",
                "P. pinnata (swamp specialist)"
        ))
 dev.off()
 
-
-
-
-# significant number of impossibly small seedlings
-# probably the result of negative growth caused by strong inter/intraspecific but they haven't had the chance to die yet (next year)
-usual$ppoS$logheight[which(usual$ppoS$logheight < 0)]
-usual$gneS$logheight[which(usual$gneS$logheight < 0)]
-usual$ppoS$logheight[which(usual$ppoS$logheight < 0)] <- NA
-usual$gneS$logheight[which(usual$gneS$logheight < 0)] <- NA
-
-
 # sample the height/dbh distributions instead of plotting all
 n=500
 
 # calculate average transition size of seedling (DBH=1cm)
+tran.parm <- read.csv("data/transition params CAA Jun21.csv", header=T, row.names=1)
+rec.parm <- t(read.csv("data/dispersal kernel parameters Apr21.csv", header=T, row.names=1))
 tran.parm["transition.height",] <- (log(1) - tran.parm["tran.int",]) / tran.parm["tran.h",]
 
-#pdf(file = "D:\\Dropbox\\twn idiwn\\Post doc\\IBM temp\\size distributions Jul21.pdf", width=16, height=9)
+#pdf(file = "D:\\Dropbox\\twn idiwn\\Post doc\\IBM temp\\size distributions 3spp Aug21.pdf", width=16, height=9)
 par(mfrow=c(1,2), mar=c(5,5,2,2))
 hist(sample(usual$ppoS$logheight, n), col=col.t[2], main="", xlab="Seedling height (log-transformed)",
      xlim=c(0,6), breaks=seq(0,8,0.5))
-hist(sample(usual$gneS$logheight, n), col=col.t[4], add=T, breaks=seq(0,8,0.5))
 hist(sample(usual$sceS$logheight, n), col=col.t[1], add=T, breaks=seq(0,8,0.5))
 hist(sample(usual$ppiS$logheight, n), col=col.t[3], add=T, breaks=seq(0,8,0.5))
 abline(v=tran.parm["transition.height", c("Gironniera.nervosa", "Prunus.polystachya", "Strombosia.ceylanica", "Pometia.pinnata")],
@@ -176,12 +152,10 @@ abline(v=rec.parm["rcsz", c("Gironniera.nervosa", "Prunus.polystachya", "Strombo
 legend('topleft', bty="n", lwd=3, lty=c(1,2), legend=c("average recruitment size", "average transition size"))
 hist(sample(usual$ppiT$logdbh, n), col=col.t[3], main="", xlab="Adult DBH (log-transformed)", 
      xlim=c(0,6), breaks=seq(0,8,0.5))
-hist(sample(usual$gneT$logdbh, n), col=col.t[4], add=T, breaks=seq(0,8,0.5))
 hist(sample(usual$sceT$logdbh, n), col=col.t[1], add=T, breaks=seq(0,8,0.5))
 hist(sample(usual$ppoT$logdbh, n), col=col.t[2], add=T, breaks=seq(0,8,0.5))
 legend('topright', fill=col.t[c(4,2,1,3)], title="Species", bty="n",
        legend=c(
-               "G. nervosa (non-swamp specialist)",
                "P. polystachya (non-swamp generalist)",
                "S. ceylanica (swamp generalist)",
                "P. pinnata (swamp specialist)"
